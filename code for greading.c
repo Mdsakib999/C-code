95bfa854e9b78e14df8cdee7253007fdc4aaa3f1
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-08-10 23:05:02 +0600

    Add files via upload

---
+++ code for greading.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+char scoreTograde (int score);
+int main(void)
+{
+    int score;
+    char grade;
+    printf("enter the test score (0-100):");
+    scanf("%d",&score);
+    grade = scoreTograde (score);
+    printf("the grade is : %c\n",grade);
+    return 0;
+}
+char scoreTograde (int score)
+{
+    char grade;
+        if (score>=90)
+            grade = 'A';
+    else if(score>=80)
+            grade = 'B';
+    else if(score>=70)
+            grade = 'C';
+    else
+        grade = 'F';
+    return grade;
+}

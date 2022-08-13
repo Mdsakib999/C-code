c90de831fb266a49e0d628acdf1cd9284b1c3914
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-08-13 21:12:50 +0600

    Add files via upload

---
+++ condition3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    if(n < 30) {
+        printf("n is less then 30\n");
+    }
+    else if(n < 50) {
+        printf ("n is less then 50\n");
+    }
+    return 0;
+}

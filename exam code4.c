a82565bb905fa98e3eacefa089d72ec077fe172a
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-08-19 10:18:46 +0600

    Add files via upload

---
+++ exam code4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main(int x, int y)
+{
+   int t;
+   t=x;
+   x=y;
+   y=t;
+   printf("%d%d\n", x,y);
+}
+ main()
+{
+    int a=3, b=4;
+    swap(a,b);
+    printf("%d%d", a,b);
+}

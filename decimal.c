335191d3d026e6426e250efc9b2993c349426610
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-08-19 10:16:59 +0600

    Add files via upload

---
+++ decimal.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    double a, b, sum;
+    a = 9.8;
+    b = 8.547;
+    sum = a + b;
+    printf("sum is: %lf\n", sum);
+    printf("sum is: %0.2lf\n" ,sum);
+
+    return 0;
+}

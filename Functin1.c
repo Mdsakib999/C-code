41de3bbb93c637c0124bd2a9c385983113b1d417
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-08-26 23:16:15 +0600

    Add files via upload

---
+++ Functin1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void print_Divi(int x)
+{
+    int i;
+    for(i=1; i<= x; i++)
+    {
+        if( x % i ==0)
+        {
+            printf("%d\n", i);
+        }
+    }
+}
+int main()
+{
+    print_Divi(329);
+    return 0;
+}

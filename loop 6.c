de601edd6f8c51817795b7863910b9b2f21fa28b
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-09-06 23:24:33 +0600

    Add files via upload

---
+++ loop 6.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n, i;
+    scanf("%d\n", &n);
+    scanf("%d", &i);
+
+    while(i <=10)
+    {
+
+        printf("%d x %d = %d\n", n, i, n*i );
+
+        i = i+1;
+    }
+    return 0;
+}
+

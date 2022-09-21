3d61979d7f18f4e1ff2bb47d97e55a29637f8486
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-09-21 21:33:00 +0600

    Add files via upload

---
+++ loop add2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int i, a, b, sum=0;
+    scanf("%d\n", &a, &b);
+    for(i=a; i<=b; i++)
+        {
+
+       printf("The number is %d\n", i);
+
+    }
+
+    return 0;
+}

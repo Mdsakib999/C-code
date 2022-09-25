87c37a2c9ab6e3b0752302ccb3b69d8525c0440d
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-09-25 20:59:52 +0600

    Add files via upload

---
+++ loop1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n=1;
+
+    printf("%d\n", n);    //or we can use printf("%d\n", n++);
+    n = n+1;
+    printf("%d\n", n);
+    n = n+1;
+    printf("%d\n", n);
+    n = n+1;
+    printf("%d\n", n);
+    n = n+1;
+    printf("%d\n", n);
+    n = n+1;
+    return 0;
+
+}

83f1dfad80ab60fdb685ca20c3eb529f53c3c764
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-09-27 21:51:55 +0600

    Add files via upload

---
+++ loop2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n=1;
+    while(n<= 100)
+    {
+        printf("%d\n", n);
+        n++;
+        if (n >  1 0)
+        {
+            break;
+        }
+    }
+    return 0;
+
+}

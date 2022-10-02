8dc463c923c8d9ef13329726c56d34354a8ef74f
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-10-02 21:18:35 +0600

    Add files via upload

---
+++ loop6.1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n, i;
+    scanf("%d\n", &n);
+    scanf("%d", &i);
+
+    for(i; i<=10; i++)
+    {
+
+        printf("%d x %d = %d\n", n, i, n*i );
+
+
+    }
+    return 0;
+}
+

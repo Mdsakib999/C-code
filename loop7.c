0e9b66c40b53c7744432dc61040fb1a8f3f537bd
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-10-03 21:17:02 +0600

    Add files via upload

---
+++ loop7.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a=0, b=3,i;             //add kora namata
+    for(i=1; i<=10; i++)
+    {
+        a= a+b;
+        printf("%d x %d =%d\n", b,i,a);
+    }
+    return 0;
+}

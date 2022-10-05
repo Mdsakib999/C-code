059e0e73e44c5bbf9f2377fde6519972c9c3edb1
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-10-05 22:00:48 +0600

    Add files via upload

---
+++ middle number.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int num1, num2, num3;
+    printf("Enter three numbers\n");
+    scanf("%d%d%d",&num1,num2,&num3);
+    if(num2>num1 && num1>num3 ||num3>num1 && num1>num2){
+        printf("\n%d is a middle number",num1);
+    }
+    if(num1>num2 && num2>num3 ||num3>num2 && num2>num1){
+        printf("\n%d is a middle number",num2);
+    }
+    if(num1>num3 && num3>num2 ||num2>num3 && num3>num1){
+        printf("\n%d is a middle number",num1);
+    }
+    getch();
+    return 0;
+}

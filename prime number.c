b7d57ce5d1b22d65bde15412392677e626d65dae
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-10-08 21:15:01 +0600

    Add files via upload

---
+++ prime number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n, i, cunt=0;
+    printf(" Enter any positive number\n");
+    scanf("%d", &n);
+    for(i=2; i<n; i++) {
+        if(n % i ==0) {
+          cunt++;
+          break;
+        }
+    }
+    if(cunt==0) {
+        printf("Prime number\n");
+    }
+    else {
+        printf("Not prime number\n");
+    }
+
+    return 0;
+}

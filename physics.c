a692e61897bad7b754d2e5dd99236e32e86b7555
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-10-08 21:11:20 +0600

    Add files via upload

---
+++ physics.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+ {
+     int t, v, s;
+         printf(" Enter the Time :");
+         scanf(" %d", &t);
+
+          printf(" Enter the velocity :");
+         scanf(" %d", &v);
+
+         s = 2*v*t;
+
+         printf("The distance is : %d\n", s);
+
+         return 0;
+
+ }

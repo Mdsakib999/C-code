52bfc2feb8d491acd2308975d686406ea2855754
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-10-10 21:45:49 +0600

    Add files via upload

---
+++ problam 1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a=1, b=2, c=3;
+    while (c>0) {
+        a = a+1;
+        b = b-1;
+        c--;
+    }
+    printf("a= %d, b= %d, c= %d", a,b,c);
+    return 0;
+}

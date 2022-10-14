01f540258422f89b21e3a2cd9dee2035b94e8e8c
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-10-14 21:28:12 +0600

    Add files via upload

---
+++ star rows.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+ {
+     int a,b,c,rows;
+
+     printf("Enter the number of rows\t");
+     scanf("%d", &rows);
+      for (a=1; a<=rows; a++)
+      {
+          //print margin spaces before the stars in each roes
+          for(b=a; b<rows; b++)
+          {
+              printf("*");
+          }
+          for (c=1; c<=rows;c++)
+          {
+              printf("*");
+          }
+          //goes to next line after each row
+          printf("\n");
+
+      }
+      return 0;
+ }

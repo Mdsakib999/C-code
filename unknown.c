f302c6619b2783d13dcdee8eceb626392d82c9a4
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-10-17 23:24:09 +0600

    Add files via upload

---
+++ unknown.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void strcat(char*s, char*t)
+{
+    while(*s)
+    s++;
+
+    while(*s++=*t++)
+    ;
+}
+void main()
+{
+    char s[]="Hello,";
+    char t[]="world";
+    strcat(s,t);
+    printf("%s",s);
+}
+

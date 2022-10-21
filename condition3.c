4d03c6beaa6110b8f42170daa59b4da9e3da2830
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-10-21 20:33:14 +0600

    Update condition3.c

--- condition3.c
+++ condition3.c
@@ -1,7 +1,12 @@
+// Check two number using condition.
+
+
 #include<stdio.h>
 int main()
 {
     int n;
+    
+    printf("Enter a number: ");
     scanf("%d", &n);
     if(n < 30) {
         printf("n is less then 30\n");

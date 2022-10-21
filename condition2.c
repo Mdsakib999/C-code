2322136f4f2b31249efb3d9e0ab3aba66bb441db
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-10-21 20:29:49 +0600

    Update condition2.c

--- condition2.c
+++ condition2.c
@@ -1,16 +1,1 @@
-#include<stdio.h>
-int main()
-{
-    int n;
-    scanf("%d", &n);
-    if (n < 0) {
-        printf("The number is negative\n");
-    }
-    else if (n > 0) {
-        printf(" The number is positive\n");
-    }
-    else if(n == 0) {
-        printf("The number is zero!\n");
-    }
-    return 0;
-}
+// Determine whether the given number is positive, negative or zero?

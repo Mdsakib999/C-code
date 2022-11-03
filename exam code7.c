14d6ab20366aba2292e0b69c7f09c764b2c01824
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-11-03 22:39:40 +0600

    Update exam code7.c

--- exam code7.c
+++ exam code7.c
@@ -1,12 +1,16 @@
+// Program using condition.
+
+
 #include<stdio.h>
 void main()
 {
    int x,y;
+   printf("Enter a number: \n");
    scanf("%d", &x);
    if(x<0)
     y=2*x+1;
    else if(x>0)
     y=2*x-1;
    else y=0;
-   printf("%d", y);
+   printf("value is: %d", y);
 }

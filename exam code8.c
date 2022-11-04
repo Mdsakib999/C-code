cc24b4105d07e1f1178568e32b631aa5c31b84d6
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-11-04 23:35:30 +0600

    Update exam code8.c

--- exam code8.c
+++ exam code8.c
@@ -4,16 +4,16 @@
 #include<stdio.h>
 void main()
 {
-    int i,a,sum=0;
+    int i, a, sum=0;
     float ave;
     printf("Enter 10 value: \n");
     for(i=1; i<10; i++)
     {
         scanf("%d", &a);
-            sum+=a;
+            sum + = a;
     }
     printf("sum is: %d\n", sum);
-    ave=(float)sum/10;
+    ave = (float)sum/10;
         printf("ave = %f\n", ave);
 
 }

5177e4e517f782bba51b1ebdf9bac22750b1bee7
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-11-15 23:48:49 +0600

    Update loop 8.1.c

--- loop 8.1.c
+++ loop 8.1.c
@@ -1,12 +1,13 @@
+
 #include<stdio.h>
 int main()
 {
-    int n, i, m=0;
-    for(n=1; n<=20; n++)
+    int n, i, m = 0;
+    for(n=1; n <= 20; n++)
         {
         for(i=1; i<=10; i++)
          {
-             m=m+n;
+             m = m + n;
             printf("%d x %d= %d\n", n, i, n*i);
 
             if(i==0) {

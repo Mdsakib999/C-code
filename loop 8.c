e1a0827ce1ee50a7941ef727eb3edd3c7b5506fe
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-11-19 22:55:47 +0600

    Update loop 8.c

--- loop 8.c
+++ loop 8.c
@@ -1,12 +1,15 @@
+// Namta of given N number
+
+
 #include<stdio.h>
 int main()
 {
     int n, i;
-    for(n=1; n<=20; n++)
+    for(n = 1; n <= 20; n++)
         {
-        for(i=1; i<=10; i++)
+        for(i = 1; i <= 10; i++)
          {
-            printf("%d x %d= %d\n", n, i, n*i);
+            printf("%d x %d = %d\n", n, i, n*i);
         }
     }
     return 0;

73afcf894afad54f5945f892942cf92f7019b34a
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-11-21 23:48:02 +0600

    Update loop add.c

--- loop add.c
+++ loop add.c
@@ -1,8 +1,12 @@
+// Print the sum of given number in loop
+// Loop problem
+
+
 #include<stdio.h>
 int main()
 {
-    int i, sum=0;
-    for(i=101; i<=201; i++)
+    int i, sum = 0;
+    for(i = 101; i<=201; i++)
         {
 
        printf("The number is %d\n", i);

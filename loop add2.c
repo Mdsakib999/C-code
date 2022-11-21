ad9a190226651c7383c2e52d7c8c28c3c2a44d3b
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-11-21 23:53:30 +0600

    Update loop add2.c

--- loop add2.c
+++ loop add2.c
@@ -1,9 +1,12 @@
+
+
+
 #include<stdio.h>
 int main()
 {
     int i, a, b, sum=0;
     scanf("%d\n", &a, &b);
-    for(i=a; i<=b; i++)
+    for(i = a; i <= b; i++)
         {
 
        printf("The number is %d\n", i);

3224a614456ae465d0e487f153b1dc6f790a3f78
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-12-07 22:32:16 +0600

    Update loop4.c

--- loop4.c
+++ loop4.c
@@ -1,11 +1,13 @@
+
+
 #include<stdio.h>
 int main()
 {
     int n = 1;
-    while(n<=100)
+    while(n <= 100)
     {
         printf("%d\n", n);
-        n = n*2;
+        n = n * 2;
     }
     return 0;
 }

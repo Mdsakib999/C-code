dda351623490bb8e3b0e43a8261e28b3fec4056e
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-12-09 23:59:07 +0600

    Update star rows.c

--- star rows.c
+++ star rows.c
@@ -1,22 +1,24 @@
 #include<stdio.h>
 int main()
  {
-     int a,b,c,rows;
+     int a, b, c, rows;
 
      printf("Enter the number of rows\t");
      scanf("%d", &rows);
-      for (a=1; a<=rows; a++)
+      for ( a = 1; a <= rows; a++ )
       {
           //print margin spaces before the stars in each roes
-          for(b=a; b<rows; b++)
+       
+          for(b = a; b<rows; b++)
           {
               printf("*");
           }
-          for (c=1; c<=rows;c++)
+          for (c = 1; c <= rows; c++)
           {
               printf("*");
           }
           //goes to next line after each row
+       
           printf("\n");
 
       }

55ae6a485228fe3a71179d926ac9d3c6e0795b74
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-12-10 23:34:12 +0600

    Update loop7.c

--- loop7.c
+++ loop7.c
@@ -1,11 +1,15 @@
+
+
+
 #include<stdio.h>
 int main()
 {
-    int a=0, b=3,i;             //add kora namata
-    for(i=1; i<=10; i++)
+    int a = 0, b = 3, i;             //add kora namata
+    for(i = 1; i <= 10; i++)
     {
-        a= a+b;
-        printf("%d x %d =%d\n", b,i,a);
-    }
+        a = a + b;
+        printf("%d x %d = %d\n", b, i, a);
+    } 
+    
     return 0;
 }

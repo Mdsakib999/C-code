2375fc98c2578ec4659e7eea2a5f548455eb1811
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-12-10 23:31:48 +0600

    Update loop6.1.c

--- loop6.1.c
+++ loop6.1.c
@@ -1,17 +1,21 @@
+
+
+
 #include<stdio.h>
 int main()
 {
     int n, i;
     scanf("%d\n", &n);
-    scanf("%d", &i);
+    scanf("%d", & i);
 
-    for(i; i<=10; i++)
+    for(i; i <= 10; i++)
     {
 
-        printf("%d x %d = %d\n", n, i, n*i );
+        printf( "%d x %d = %d\n", n, i, n*i );
 
 
     }
+    
     return 0;
 }
 

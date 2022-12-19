2e5c717261bd8e155c1480212dbc3a323b078c86
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-12-19 22:12:26 +0600

    Update same of numbar.c

--- same of numbar.c
+++ same of numbar.c
@@ -1,1 +1,19 @@
 
+
+
+#include <stdio.h>
+float temp_fahrenheit;     
+float temp_celsius;     
+char line_text[50];        
+
+int main() {
+	printf("Input a temperature in Celsius: ");
+	fgets(line_text, sizeof(line_text), stdin);
+	sscanf(line_text, "%f", &temp_celsius);
+
+	temp_fahrenheit = ((9.0 / 5.0) * temp_celsius) + 32.0;
+	
+	printf("%f degrees Fahrenheit.\n", temp_fahrenheit);
+
+	return(0);
+}

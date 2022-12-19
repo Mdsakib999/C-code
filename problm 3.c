3e2c77783d56c326bee9a196fbc21acc1ded3869
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-12-19 22:21:41 +0600

    Update problm 3.c

--- problm 3.c
+++ problm 3.c
@@ -1,14 +1,19 @@
+// Printing number
+
+
 #include<stdio.h>
 #define N 20
 fun(int a[], int n, int m)
 {
     int i, j;
-    for(i=m; i>=n; i--)a[i+1]=a[i];
+    for(i = m; i >= n; i--) a[i+1] =a[i];
 
 }
 main()
+
 {
-    int i,a[N]={1,2,3,4,5,6,7,8,9,10};
-    fun (a,2,9);
-    for(i=0;i<5; i++)printf("%d",a[i]);
+    int i,a[N] = {1,2,3,4,5,6,7,8,9,10};
+    fun (a, 2, 9);
+    for(i=0; i<5; i++)
+    printf("%d", a[i]);
 }

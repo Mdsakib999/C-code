87f431348e13b412bc7d9f04c92124919c288ca0
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-12-24 23:44:06 +0600

    Rename problem1.c to problem1.1.c

--- problem1.1.c
+++ problem1.1.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main()
 {
-    int a=1, b=2, c=3;
-    while (c>0) {
-        a = a+1;
-        b = b-1;
+    int a = 1, b = 2, c = 3;
+    while (c > 0) {
+        a = a + 1;
+        b = b - 1;
         c--;
     }
     printf("a= %d, b= %d, c= %d", a,b,c);

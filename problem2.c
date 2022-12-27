3ea6d5e9afb7bcbf3166dfad76deeef1e4584ba3
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-12-27 22:36:55 +0600

    Update problem2.c

--- problem2.c
+++ problem2.c
@@ -7,12 +7,13 @@
 void main()
 {
     char str[100]; /* Declares a string of size 100 */
-    int l= 0;
+    int l = 0;
 	
        printf("\n\nFind the length of a string :\n");
-       printf("---------------------------------\n"); 	
+ 	
        printf("Input the string : ");
        fgets(str, sizeof str, stdin);
+	
     while(str[l]!='\0')
     {
         l++;
@@ -32,12 +33,12 @@ void main()
 int main()
 {
     int i,j;
-    for(i=0; i<5; i++) {        /* 1ta porjonto star anta caila
+    for(i = 0; i < 5; i++) {        /* 1ta porjonto star anta caila
                                   i ar man 1 baraita hoba i<6 */
-        for(j=0; j<=5-i; j++) {
+        for(j = 0; j <= 5-i; j++) {
             printf(" ");        //this line print 1 space
-        }
-        for(j=5; j>=i; j--) {
+        } 
+        for(j = 5; j >= i; j--) {
             printf("*");        /*this line print 1 star */
         }
         printf("\n");

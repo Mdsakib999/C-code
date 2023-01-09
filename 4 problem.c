481b445b84d4fad63d38676587375b469a674fbf
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-01-09 23:45:52 +0600

    Update 4 problem.c

--- 4 problem.c
+++ 4 problem.c
@@ -3,13 +3,13 @@
 #include<stdio.h>
 int strend(char*s, char*t)
 {
-    char*bs=s;
-    char*bt=t;
-    for(;*s;s++)
+    char*bs = s;
+    char*bt = t;
+    for(;*s; s++)
     ;
-    for(;*t;t++)
+    for(;*t; t++)
     ;
-    for(;*s==*t;s--,t--)
+    for(;*s==*t; s--,t--)
     if(t==bt||s==bs)
     break;
     if(*s==*t && t==bt*s!='\0')

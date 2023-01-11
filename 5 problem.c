b86bf1ddb93322f64e81235b470c172918616df4
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-01-11 23:45:57 +0600

    Update 5 problem.c

--- 5 problem.c
+++ 5 problem.c
@@ -1,1 +1,19 @@
 
+
+
+
+
+#include <stdio.h>
+
+int main(void)
+{
+    int d,y,m,sum;
+    printf("Enter number of days: ");
+    scanf("%d",&d);
+    y = d / 365;
+    d = d % 365;
+    m = d / 30;
+    sum = d % 30;
+    printf("\n%d years, %d months, %d days\n",y,m,sum);
+    return 0;
+}

6db0b54d6a40fa1f319e9696ff93c5763214a696
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-02-14 23:10:37 +0600

    Commit

--- 23 problem.c
+++ 23 problem.c
@@ -1,2 +1,20 @@
 
 // Write a C Program to find out the sum of first n numbers??
+
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+  int i, num, sum=0;
+  printf("Enter a number : ");
+  scanf("%d", &num);
+  for(i = 0; i<num; i++)
+  {
+  sum=sum+i;
+  }
+  printf("\n The sum of numbers = %d",sum);
+
+  return 0;
+}

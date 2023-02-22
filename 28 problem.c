fbecf78deebf3fe989014bd2859808cd5b18dc56
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-02-22 23:38:09 +0600

    Commit

--- 28 problem.c
+++ 28 problem.c
@@ -4,13 +4,14 @@
 int main()
 {
     int a[10]={1, 5, 5, 7, 2, 8, 6, 9, 3,8},sum,i,Ave;
-    for(i=0; i<=9; i++) {
+    for(i= 0; i <= 9; i++) {
 
         printf("%d", a[i]);
-        sum = sum +i;
+        sum = sum + i;
 
     }
     printf("The sum is %d\n", sum);
-    Ave=sum/10;
+    Ave = sum / 10;
+    
     printf("%d\n", Ave);
 }

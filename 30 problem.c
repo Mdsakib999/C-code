312c8e94cab59d8e63994793ce556bf6f7eaf6ef
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-02-25 22:59:53 +0600

    Commit

--- 30 problem.c
+++ 30 problem.c
@@ -3,17 +3,17 @@
 #include<stdio.h>
 int main()
 {
-    int a[10], sum=0, i;
+    int a[10], sum = 0, i;
 
     printf(" enter the value : \n");
 
-    for (i= 0; i<10; i++)
+    for (i = 0; i < 10; i++)
     {
         scanf("%d\n", &a[i]);
 
     }
 
-    for (i= 0; i<10; i++)
+    for (i = 0; i<10; i++)
     {
         sum = sum + a[i];
     }

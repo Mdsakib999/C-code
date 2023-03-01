7a8227d249716045752153c804c6ded5706aefd7
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-03-01 23:59:59 +0600

    commit

--- 35 problem.c
+++ 35 problem.c
@@ -15,11 +15,11 @@ int main(void)
 char scoreTograde (int score)
 {
     char grade;
-        if (score>=90)
+        if (score >= 90)
             grade = 'A';
-    else if(score>=80)
+    else if(score >= 80)
             grade = 'B';
-    else if(score>=70)
+    else if(score >= 70)
             grade = 'C';
     else
         grade = 'F';

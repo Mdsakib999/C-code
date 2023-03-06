437c9b41547bf578b005b219ee79bbdfb3e60620
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-03-06 23:01:03 +0600

    commit

--- exam code4.c
+++ exam code4.c
@@ -4,14 +4,14 @@
 void main(int x, int y)
 {
    int t;
-   t=x;
-   x=y;
-   y=t;
-   printf("%d%d\n", x,y);
+   t = x;
+   x = y;
+   y = t;
+   printf("%d %d\n", x, y);
 }
  main()
 {
-    int a=3, b=4;
+    int a = 3, b = 4;
     swap(a,b);
-    printf("%d%d", a,b);
+    printf("%d %d", a, b);
 }

fdad338cf74b62a16ebeb6f15ec804750b87e1b0
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-03-08 23:59:13 +0600

    Update exam code7.c

--- exam code7.c
+++ exam code7.c
@@ -4,13 +4,13 @@
 #include<stdio.h>
 void main()
 {
-   int x,y;
+   int x, y;
    printf("Enter a number: \n");
    scanf("%d", &x);
-   if(x<0)
-    y=2*x+1;
-   else if(x>0)
-    y=2*x-1;
-   else y=0;
+   if(x < 0)
+    y=2 * x + 1;
+   else if( x > 0)
+    y= 2 * x - 1;
+   else y = 0;
    printf("value is: %d", y);
 }

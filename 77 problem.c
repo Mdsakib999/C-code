bad7006fb8c9030e3bfd0f730d6e71e5462d1316
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-05-06 22:33:57 +0600

    Update 77 problem.c

--- 77 problem.c
+++ 77 problem.c
@@ -1,1 +1,21 @@
 // 
+
+
+
+
+#include <stdio.h>
+void main()
+{
+    float PerHeight;
+ 
+    printf("Input the height in centimetres :");
+    scanf("%f", &PerHeight);
+    if (PerHeight < 150.0)
+        printf("The person is Dwarf. \n");
+    else if ((PerHeight >= 150.0) && (PerHeight < 165.0))
+        printf("The person is  average heighted. \n");
+    else if ((PerHeight >= 165.0) && (PerHeight <= 195.0))
+        printf("The person is taller. \n");
+    else
+        printf("Abnormal height.\n");
+}

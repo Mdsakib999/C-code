aebecb726642e420147e6b82ef02d069bc7526b5
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-07-18 22:43:09 +0600

    commit

--- 78 problem.c
+++ 78 problem.c
@@ -1,3 +1,34 @@
 // Program to find occurrence of a character in an input string in C.
 
-  
+  #include <stdio.h>
+#define MAX	100
+
+int main()
+{
+	char str[MAX]={0};
+	char ch;
+	int count,i;
+	
+
+	printf("Enter a string: ");
+	scanf("%[^\n]s",str); //read string with spaces
+	
+	getchar(); // get extra character (enter/return key)
+	
+	//input character to check frequency
+	printf("Enter a character: ");
+	ch=getchar();
+	
+	//calculate frequency of character 
+	count=0;
+	for(i=0; str[i]!='\0'; i++)
+	{
+		if(str[i]==ch)
+			count++;
+	}
+	
+	printf("\'%c\' found %d times in \"%s\"\n",ch,count,str);
+	
+	return 0;	
+}
+

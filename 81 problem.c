7a594869391026eb39de7bac119a2703b214f780
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-10-09 20:55:26 +0600

    commit

--- 81 problem.c
+++ 81 problem.c
@@ -1,1 +1,20 @@
 // C program to calculate the area of Cube?
+
+#include <stdio.h>
+
+float calculateAreaOfCube(float side) {
+    return 6.0 * side * side;
+}
+
+int main() {
+    float side, area;
+
+    printf("Enter the length of side: ");
+    scanf("%f", &side);
+
+    area = calculateAreaOfCube(side);
+
+    printf("Area of Cube is: %f\n", area);
+
+    return 0;
+}

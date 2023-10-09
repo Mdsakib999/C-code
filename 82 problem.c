d27bfd27462b0042a0a306a996fe2339b3879340
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-10-09 21:11:39 +0600

    commit

--- 82 problem.c
+++ 82 problem.c
@@ -1,2 +1,23 @@
 // C program to calculate the value of nCr??
 
+#include <stdio.h>
+
+int factorial(int n) {
+    return (n == 0) ? 1 : n * factorial(n - 1);
+}
+
+int main() {
+    int n, r;
+
+    printf("Enter the value of N: ");
+    scanf("%d", &n);
+
+    printf("Enter the value of R: ");
+    scanf("%d", &r);
+
+    int nCr = factorial(n) / (factorial(r) * factorial(n - r));
+
+    printf("The nCr is: %d\n", nCr);
+
+    return 0;
+}

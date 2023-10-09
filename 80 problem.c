960ff9cd7a27dba4fdc0013f3f7e1f074e1921de
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-10-09 20:35:06 +0600

    commit

--- 80 problem.c
+++ 80 problem.c
@@ -1,1 +1,22 @@
 // C program to calculate the area of Trapezium???
+
+
+#include <stdio.h>
+
+float calculateAreaOfTrapezium(float base1, float base2, float height) {
+    return 0.5 * (base1 + base2) * height;
+}
+
+int main() {
+    float base1, base2, height, area;
+
+    printf("Enter base1, base2, and height: ");
+    scanf("%f %f %f", &base1, &base2, &height);
+
+    area = calculateAreaOfTrapezium(base1, base2, height);
+
+    printf("Area of Trapezium is: %f\n", area);
+
+    return 0;
+}
+
